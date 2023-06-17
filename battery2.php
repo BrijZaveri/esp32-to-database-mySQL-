577c90137fdaae3f106d7f96977ff23f77ee7f83
Brij Zaveri <91523750+BrijZaveri@users.noreply.github.com>
2023-06-17 15:53:01 +0530

    Battery_data_logger.php

--- battery2.php
+++ battery2.php
@@ -1,24 +1,44 @@
 <?php
-class gt_can_voltage{
- public $link='';
- function __construct($voltage){
-  $this->connect();
-  $this->storeInDB($voltage);
- }
- 
- function connect(){
-  $this->link = mysqli_connect('localhost','root','joyebike@1234') or die('Cannot connect to the DB');
-  mysqli_select_db($this->link,'battery') or die('Cannot select the DB');
- }
- 
- function storeInDB($voltage){
-  $query = "INSERT into gt_can_voltage SET voltage='".$voltage."'";
-  $result = mysqli_query($this->link,$query) or die('Errant query:  '.$query);
- }
- 
+
+// Connect to MySQL database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "battery";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
-if(isset($_GET['voltage']) && !empty($_GET['voltage'])){
- $gt_can_voltage=new gt_can_voltage($_GET['voltage']);
+// Get the values from the HTTP GET request
+$id = isset($_GET["id"]) ? $_GET["id"] : null;
+$voltage = isset($_GET["voltage"]) ? $_GET["voltage"] : null;
+$current = isset($_GET["current"]) ? $_GET["current"] : null;
+$soc = isset($_GET["soc"]) ? $_GET["soc"] : null;
+$remainingAh = isset($_GET["remainingAh"]) ? $_GET["remainingAh"] : null;
+$temp1 = isset($_GET["temp1"]) ? $_GET["temp1"] : null;
+$temp2 = isset($_GET["temp2"]) ? $_GET["temp2"] : null;
+$temp3 = isset($_GET["temp3"]) ? $_GET["temp3"] : null;
+$temp4 = isset($_GET["temp4"]) ? $_GET["temp4"] : null;
+$temp5 = isset($_GET["temp5"]) ? $_GET["temp5"] : null;
+$serialnumber = isset($_GET["serialnumber"]) ? $_GET["serialnumber"] : null;
+
+// Insert the values into the database
+$sql = "INSERT INTO gt_can_data (id, voltage, current, soc, remainingAh, temp1, temp2, temp3, temp4, temp5, serialnumber) 
+        VALUES ('$id', '$voltage', '$current', '$soc', '$remainingAh', '$temp1', '$temp2', '$temp3', '$temp4', '$temp5', '$serialnumber')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Data inserted successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
+
+// Close the database connection
+$conn->close();
+
 ?>
+<?php
